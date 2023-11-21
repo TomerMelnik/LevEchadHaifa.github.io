cb2c514e65621225283f1f1917d2664bd74e31b0
tomer melnik <tomermelnik@gmail.com>
2023-11-21 19:33:49 +0200

    fixes to deploy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
+import React from 'react';
+
 import { AppProps } from 'next/app';
 
 import '../styles/main.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <Component {...pageProps} as ComponentType />
 );
 
 export default MyApp;
